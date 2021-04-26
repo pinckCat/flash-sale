1ddc083089b5e4695c11dd5de837717b7cff63df
黄生斌 <jangohuang@MacBook-Pro.local>
2021-04-26 19:40:51 +0800

    add reconnect function to fix the error of connect fail when program run before than mysql container and fix some bug

--- routes/flash-sale-consumer.js
+++ routes/flash-sale-consumer.js
@@ -15,48 +15,8 @@ var mysql = require('mysql'),
 const redis = require("redis");
 const redisClient = redis.createClient(6379, "81.70.204.243");
 
-//库存量
-const inStock = 2000
-var messageOffset = 0;
-
-//定义一个队列保存数据，kafka consumer 收到数据后,将对应的数据插入mysql数据库
-var q = async.queue(function (message, callback) {
-    let orderInfo = JSON.parse(message.value);
-    mysqlPool.getConnection(function (err, conn) {
-        if (err) throw err;
-        conn.query("INSERT INTO orders SET ?", {
-            user_id: orderInfo.user_id,
-            address_id: orderInfo.address_id,
-            product_name: orderInfo.product_name,
-            product_price: orderInfo.product_price,
-            status: orderInfo.status
-        }, function (error, results, fields) {
-            conn.release();
-            if (error) {
-                throw error;
-            }
-            callback();
-        });
-    });
-}, 2);
-
-function flashConsumer() {
-    mysqlPool.getConnection(function (err, conn) {
-        if (err) throw err;
-        //获取数据库已存入的数据量，避免获取重复数据
-        conn.query("SELECT MAX(id) AS offset from orders", function (errors, results, fields) {
-            conn.release();
-            if (errors) throw errors;
-            else {
-                if (results[0].offset != null) {
-                    messageOffset = results[0];
-                }
-            }
-
-        });
-    });
-
-    //查询缓存的库存值，没有对应的key或者值小于等于0，则初始化一个值
+redisClient.on("ready", function (err, reply) {
+    //ready的时候查询缓存的库存值，没有对应的key或者值小于等于0，则初始化一个值
     redisClient.get("inStock", function (err, res) {
         if (res === null || res <= 0) {
             redisClient.set("inStock", inStock, function (err, res) {
@@ -65,9 +25,73 @@ function flashConsumer() {
                 redisClient.end(true);
                 console.log("初始化库存值成功！库存为： "+ inStock);
             });
-        };
+        }
     });
+});
 
+//货物库存量
+const inStock = 2000
+
+var messageOffset = 0;
+
+//定义一个队列保存数据，kafka consumer 收到数据后,将对应的数据插入mysql数据库
+var q = async.queue(function (message, callback) {
+    let orderInfo = JSON.parse(message.value);
+    mysqlPool.getConnection(function (err, conn){
+        if (err) console.error(err);
+        else {
+            conn.query("INSERT INTO orders SET ?", {
+                user_id: orderInfo.user_id,
+                address_id: orderInfo.address_id,
+                product_name: orderInfo.product_name,
+                product_price: orderInfo.product_price,
+                status: orderInfo.status
+            }, function (error, results, fields) {
+                conn.release();
+                if (error) {
+                    throw error;
+                }
+                callback();
+            });
+        }
+    });
+}, 2);
+
+//mysql最大重连次数
+let mysql_max_retry_times = 5
+
+//mysql重连操作，为了避免程序先启动而mysql容器还没有运行起来连接失败的情况
+function handleMysqlDisconnect() {
+    mysqlPool.getConnection(function (err, conn) {
+      //如果连接出错，进行重连
+      if (err && mysql_max_retry_times) {
+          console.log("error when connecting to mysql: "+ err);
+          setTimeout(handleMysqlDisconnect, 5000);
+          mysql_max_retry_times -= 1;
+      } else if (err && mysql_max_retry_times < 0) {
+          console.log("have try reconnect for 3 times, program exit");
+          process.exit(0);
+      } else {
+          console.log("连接mysql数据库成功！");
+          //获取数据库已存入的数据量，避免获取重复数据
+          conn.query("SELECT MAX(id) AS offset from orders", function (errors, results, fields) {
+              conn.release();
+              if (errors) throw errors;
+              else {
+                  console.log("查询数据库,成功初始化kafka消息offset");
+                  if (results[0].offset != null) {
+                      messageOffset = results[0];
+                  }
+              }
+          });
+          return;
+      }
+    });
+};
+
+function flashConsumer() {
+    //查询数据库，初始化kafka message offset
+    handleMysqlDisconnect();
 
     let consumer = new Consumer(
         kafkaClient,
@@ -88,7 +112,7 @@ function flashConsumer() {
                 partitions: 1,
                 replicationFactor: 1
             }], (error, result) => {
-                if (error) console.log(error);
+                if (error) console.error("创建主题失败！");
             })
         };
     })
--- routes/flash-sale.js
+++ routes/flash-sale.js
@@ -11,43 +11,51 @@ var kafka = require("kafka-node"),
 const redisClient = redis.createClient(6379, "81.70.204.243");
 
 kafkaClient.on('error', function (err) {
-    console.error(err);
-})
+    throw err;
+});
 
 redisClient.on("error", function (error) {
-    console.error(error);
     throw error;
-})
+});
+
+let redisReady = false;
+
+redisClient.on("ready", function (reply) {
+    redisReady = true;
+});
 
 router.post("/", function (req, res) {
-    redisClient
-        .multi()
-        .decr("inStock")
-        .exec(function (err, replies) {
-            if (err) {
-                //断开redis连接
-                redisClient.end(true);
-                throw err;
-            } else {
-                if (replies[0] >= 0) {
-                    let payload = [{
-                        topic: 'flash-order',
-                        messages: JSON.stringify({"user_id": 1, "address_id": 1, "product_name": '买个锤子', "product_price": 9.9, "status": 0}),
-                        partition: 0
-                    }];
-                    producer.send(payload, function (err, data) {
-                        if (err) {
-                            console.error(err);
-                            throw err;
-                        }
-                        console.log('购买成功，还是剩'+replies[0]+'个');
-                    });
+    if (redisReady) {
+        redisClient
+            .multi()
+            .decr("inStock")
+            .exec(function (err, replies) {
+                if (err) {
+                    //断开redis连接
+                    redisClient.end(true);
+                    throw err;
                 } else {
-                    console.log('抢完了，下次再来');
+                    if (replies[0] >= 0) {
+                        let payload = [{
+                            topic: 'flash-order',
+                            messages: JSON.stringify({"user_id": 1, "address_id": 1, "product_name": '买个锤子', "product_price": 9.9, "status": 0}),
+                            partition: 0
+                        }];
+                        producer.send(payload, function (err, data) {
+                            if (err) {
+                                console.error(err);
+                            }
+                            console.log('购买成功，还是剩'+replies[0]+'个');
+                        });
+                    } else {
+                        console.log('抢完了，下次再来');
+                    }
                 }
-            }
-        });
-    res.send('respond with a resource');
+            });
+        res.send('respond with a resource');
+    } else {
+        res.send('not ready, please try again later');
+    }
 });
 
 module.exports = router;
